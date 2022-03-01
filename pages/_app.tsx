4b96c239ee4d5b87e04d3f4f4ccb1f602295b4af
Viktoriya Derkach <queenviktoriyphp@gmail.com>
2022-03-01 21:28:38 +0200

    document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from "next/dist/shared/lib/router/router";
+import Head from "next/head";
+import React from "react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My top</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
