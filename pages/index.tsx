d663209ef42fe13562f999554b1c04266a693b29
Viktoriya Derkach <queenviktoriyphp@gmail.com>
2022-03-03 17:54:18 +0200

    tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,58 @@
-import { Button, Htag, Paragraph } from "../components";
+import { useState } from "react";
+import { Button, Htag, Paragraph, Tag } from "../components";
 
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
   return (
     <>
-      <Htag tag="h1">Text</Htag>
-      <Button appearance="primary" arrow="right">
+      <Htag tag="h1">{counter}</Htag>
+      <Button
+        appearance="primary"
+        arrow="right"
+        onClick={() => setCounter((x) => x + 1)}
+      >
         jijiji
       </Button>
       <Button appearance="ghost" arrow="right">
         jijiji
       </Button>
-      <Paragraph size="m">
+      <Paragraph size="l">
         Студенты освоят не только hard skills, необходимые для работы
         веб-дизайнером, но и soft skills — навыки, которые позволят эффективно
         взаимодействовать в команде с менеджерами, разработчиками и
         маркетологами. Выпускники факультета могут успешно конкурировать с
         веб-дизайнерами уровня middle.
       </Paragraph>
+      <Paragraph>
+        Студенты освоят не только hard skills, необходимые для работы
+        веб-дизайнером, но и soft skills — навыки, которые позволят эффективно
+        взаимодействовать в команде с менеджерами, разработчиками и
+        маркетологами. Выпускники факультета могут успешно конкурировать с
+        веб-дизайнерами уровня middle.
+      </Paragraph>
+      <Paragraph size="s">
+        Студенты освоят не только hard skills, необходимые для работы
+        веб-дизайнером, но и soft skills — навыки, которые позволят эффективно
+        взаимодействовать в команде с менеджерами, разработчиками и
+        маркетологами. Выпускники факультета могут успешно конкурировать с
+        веб-дизайнерами уровня middle.
+      </Paragraph>
+
+      <Tag size="s" color="red">
+        tag
+      </Tag>
+      <Tag size="m" color="green">
+        tag
+      </Tag>
+      <Tag size="m" color="ghost">
+        tag
+      </Tag>
+      <Tag size="m" color="gray">
+        tag
+      </Tag>
+      <Tag size="m" color="primary">
+        tag
+      </Tag>
     </>
   );
 }
