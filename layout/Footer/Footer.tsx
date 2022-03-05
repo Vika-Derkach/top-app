fa18d6bcac1547496b9b6c5df0e7596d0045716d
Viktoriya Derkach <queenviktoriyphp@gmail.com>
2022-03-05 20:59:08 +0200

    footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,15 @@
-import cn from "classnames";
 import React from "react";
 import styles from "./Footer.module.css";
 import { FooterProps } from "./Footer.props";
 
 export const Footer = ({ ...props }: FooterProps): JSX.Element => {
   return (
-    <div className={cn(styles.paragraph)} {...props}>
-      Footer
+    <div {...props}>
+      <div className={styles.footer}>
+        <div>OwlTop © 2020 - 2021 Все права защищены</div>
+        <div>Пользовательское соглашение</div>
+        <div>Политика конфиденциальности</div>
+      </div>
     </div>
   );
 };
