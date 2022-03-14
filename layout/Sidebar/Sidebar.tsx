8c8a0b3794c006df786dd49d1efb4f4782eb3747
Viktoriya Derkach <queenviktoriyphp@gmail.com>
2022-03-14 10:21:43 +0200

    sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,13 +1,15 @@
 import cn from "classnames";
 import React from "react";
+import Logo from "../logo.svg";
 import { Menu } from "../Menu/Menu";
 import styles from "./Sidebar.module.css";
 import { SidebarProps } from "./Sidebar.props";
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
-    <div className={cn(styles.paragraph)} {...props}>
-      Sidebar
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>Search</div>
       <Menu />
     </div>
   );
