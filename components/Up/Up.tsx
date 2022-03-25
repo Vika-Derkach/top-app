6c1463c720085877084c7b01c62b53c67357f2a1
Viktoriya Derkach <queenviktoriyphp@gmail.com>
2022-03-25 21:13:59 +0200

    denymic icon

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -2,8 +2,8 @@ import cn from "classnames";
 import { motion, useAnimation } from "framer-motion";
 import React, { useEffect } from "react";
 import { useScrollY } from "../../hooks/useScrollY";
+import { ButtonIcon } from "../ButtonIcon/ButtonIcon";
 import styles from "./Up.module.css";
-import UpIcon from "./up.svg";
 
 export const Up = (): JSX.Element => {
   const controls = useAnimation();
@@ -22,13 +22,12 @@ export const Up = (): JSX.Element => {
   };
 
   return (
-    <motion.button
+    <motion.div
       className={cn(styles.up)}
-      onClick={scrollToTop}
       animate={controls}
       initial={{ opacity: 0 }}
     >
-      <UpIcon />
-    </motion.button>
+      <ButtonIcon appearance="primary" icon="up" onClick={scrollToTop} />
+    </motion.div>
   );
 };
